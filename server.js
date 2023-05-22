055e447652fdc033730d78bd114452b95aa3b349
akarshabhardwaj <akarshabhardwaj7280976503@gmail.com>
2023-05-22 23:00:37 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults({ noCors: true });
+const middlewares = jsonServer.defaults({ noCors: false });
 const port = process.env.PORT || 8080; // <== You can change the port
 
 server.use(middlewares);
